b696a8d40d18653fda281ae32136538a928e93f6
Adarsh Ramesh Vishwakarma <adarshvishwakarama232774@gmail.com>
2024-07-13 14:42:54 +0530

    All file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import BasketballPlayerCard from './components/BasketballPlayerCard';
+import lebronImage from './img/download.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <BasketballPlayerCard
+              name="LeBron James"
+              image={lebronImage}
+              position="Forward"
+              stats={{ pointsPerGame: 25.4, assistsPerGame: 7.1, reboundsPerGame: 10.5 }}
+          />
+      </div>
   );
 }
 
